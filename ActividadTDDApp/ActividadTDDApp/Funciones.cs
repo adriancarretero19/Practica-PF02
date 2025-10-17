d0f069c981ec000f1e8f64de2c39b2f4f664a725
yerko3 <yerkoclaros7@gmail.com>
2025-10-17 15:58:40 +0200

    funcion1 hecha

--- ActividadTDDApp/ActividadTDDApp/Funciones.cs
+++ ActividadTDDApp/ActividadTDDApp/Funciones.cs
@@ -2,5 +2,15 @@
 {
     public class Funciones
     {
+        public long CalcularFactorial(int n)
+        {
+            if (n < 0)
+                return -1;
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            return n * CalcularFactorial(n - 1);
+        }
     }
 }
