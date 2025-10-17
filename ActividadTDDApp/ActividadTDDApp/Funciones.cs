69e631b86dc75fe4e5a19b492a68b16f3bb4be2c
adriancarretero19 <carreteromateoadrian@gmail.com>
2025-10-17 15:57:04 +0200

    funcion2 hecha

--- ActividadTDDApp/ActividadTDDApp/Funciones.cs
+++ ActividadTDDApp/ActividadTDDApp/Funciones.cs
@@ -2,5 +2,16 @@
 {
     public class Funciones
     {
+        public bool EsContrasenyaValida(string contrasenya)
+        {
+            for (int i = 0; i < contrasenya.Length; i++)
+            {
+                if (contrasenya[i] == '#')
+                    break;
+                if (i == contrasenya.Length - 1)
+                    return false;
+            }
+            return !string.IsNullOrEmpty(contrasenya) && contrasenya.Length >= 8;
+        }
     }
 }
